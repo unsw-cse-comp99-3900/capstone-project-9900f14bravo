4cf51a2e4813cb0caf6600c1042b9c6935be9bd6
banlingbo <64313505+banlingbo@users.noreply.github.com>
2024-07-15 12:43:36 +1000

    Add files via upload

---
+++ project/frontend/src/compoment/ProcessedResult.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Typography, Button, Box } from '@mui/material';
+
+const ProcessedResult = ({ processedFile, downloadFile, handleReset, algorithm }) => {
+  const getPlotImage = () => {
+    if (algorithm === 'PIWAS' && processedFile.plot_file) {
+      return (
+        <Box sx={{ mt: 2 }}>
+          <Typography variant="h6">PIWAS Plot:</Typography>
+          <img
+            src={`http://localhost:8000/media/PIWAS-result/piwas_plot.png`}
+            alt="PIWAS Plot"
+            style={{ width: '100%', maxHeight: '500px', objectFit: 'contain' }}
+          />
+        </Box>
+      );
+    } else if (algorithm === 'PIWAS+PIE' && processedFile.plot_file) {
+      return (
+        <Box sx={{ mt: 2 }}>
+          <Typography variant="h6">PIWAS+PIE Plot:</Typography>
+          <img
+            src={`http://localhost:8000${processedFile.plot_file}`}
+            alt="PIWAS+PIE Plot"
+            style={{ width: '100%', maxHeight: '500px', objectFit: 'contain' }}
+          />
+        </Box>
+      );
+    } else if (algorithm === 'PIE' && processedFile.pie_plot) {
+      return (
+        <Box sx={{ mt: 2 }}>
+          <Typography variant="h6">PIE Plot:</Typography>
+          <img
+            src={`http://localhost:8000${processedFile.plot_file}`}
+            alt="PIE Plot"
+            style={{ width: '100%', maxHeight: '500px', objectFit: 'contain' }}
+          />
+        </Box>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <div>
+      {processedFile && (
+        <div>
+          <Typography>Process completed. Download the result below:</Typography>
+          {Object.keys(processedFile).map((key, index) => (
+            <Button
+              key={index}
+              onClick={() => downloadFile(processedFile[key])}
+              variant="contained"
+              sx={{ mt: 2, mr: 2 }}
+            >
+              Download {key.replace('_', ' ')}
+            </Button>
+          ))}
+          {getPlotImage()}
+          <Box sx={{ mt: 2 }}>
+            <Button variant="contained" color="primary" onClick={handleReset}>
+              New Pipeline
+            </Button>
+          </Box>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProcessedResult;
